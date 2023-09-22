8b5f111f228557abbe52dca340eba13d12f69b06
Toby Dylan Hocking <toby.hocking@r-project.org>
2023-09-22 10:03:45 -0700

    winners

--- homeworks/02-04-boggle/04_judge.R
+++ homeworks/02-04-boggle/04_judge.R
@@ -1,9 +1,8 @@
 library(data.table)
 library(ggplot2)
 setwd("~/teaching/2023-08-artificial-intelligence/homeworks/02-04-boggle/")
-(py.file.vec <- Sys.glob("04_judge/*.py"))
 board.txt <- "competition_board.txt"
-board.size <- 5
+board.size <- 6
 python <- "~/.local/share/r-miniconda/envs/2023-08-artificial-intelligence/bin/python"
 system(paste(python, "04_generator.py", board.size, board.txt))
 words.txt <- sub(".txt", "_words.txt", board.txt)
@@ -11,7 +10,7 @@ system(paste("python 04_solution.py", board.txt))
 solution.txt <- sub(".txt", "_solution.txt", board.txt)
 read.words <- function(txt.file){
   wdt <- tryCatch({
-    data.table::fread(txt.file, col.names="word", header=FALSE)
+    data.table::fread(txt.file, col.names="word", header=FALSE,na.strings=NULL)
   }, error=function(e){
     data.table()
   })
@@ -22,6 +21,7 @@ read.words <- function(txt.file){
 }
 solution.dt <- read.words(solution.txt)
 
+(py.file.vec <- Sys.glob("04_judge/*.py"))
 result.dt.list <- list()
 for(py.file in py.file.vec){
   py.cmd <- paste(python, py.file, board.txt, "twl06.txt")
@@ -47,8 +47,11 @@ setkey(zero.err, median)
 zero.err[, Program := factor(program, unique(program))]
 
 ggplot()+
-  theme(text=element_text(size=20))+
+  ggtitle(paste("board size=",board.size))+
+  theme(text=element_text(size=40))+
   geom_point(aes(
     seconds, Program),
-    data=zero.err)
+    shape=1,
+    data=zero.err)+
+  scale_x_log10()
     
